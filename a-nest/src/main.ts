886e2a444cce282b4bcbb133baad2062303560dd
100milliongold <gadian88@gmail.com>
2022-09-30 02:48:10 +0900

    API 문서 만들기(Swagger)

--- a-nest/src/main.ts
+++ a-nest/src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
 import passport from 'passport';
@@ -8,6 +9,16 @@ declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Sleact API')
+    .setDescription('Sleact 개발을 위한 API 문서 입니다.')
+    .setVersion('1.0')
+    .addCookieAuth('connect.sid')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   const port = process.env.PORT || 3000;
 
   app.use(cookieParser());
--- a-nest/src/users/users.controller.ts
+++ a-nest/src/users/users.controller.ts
@@ -1,24 +1,46 @@
 import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
 import { JoinRequestDto } from './dto/join.request.dto';
 import { UsersService } from './users.service';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { UserDto } from 'src/common/dto/user.dto';
 
+@ApiTags('USER')
 @Controller('api/users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
+  @ApiResponse({
+    status: 200,
+    type: UserDto,
+    description: '성공',
+  })
+  @ApiResponse({
+    status: 500,
+    type: UserDto,
+    description: '서버에러',
+  })
+  @ApiOperation({ summary: '내 정보 조회' })
   @Get()
   getUsers(@Req() req) {
     return req.user;
   }
 
+  @ApiOperation({ summary: '회원가입' })
   @Post()
   postUsers(@Body() data: JoinRequestDto) {
     this.usersService.postUsers(data.email, data.nickname, data.password);
   }
 
+  @ApiResponse({
+    status: 200,
+    description: '성공',
+    type: UserDto,
+  })
+  @ApiOperation({ summary: '로그인' })
   @Post('login')
   login() {}
 
+  @ApiOperation({ summary: '로그아웃' })
   @Post('logout')
   logOut(@Req() req, @Res() res) {
     req.logOut();
