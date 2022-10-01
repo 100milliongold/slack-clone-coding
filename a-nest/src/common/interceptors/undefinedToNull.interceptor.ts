a29acfb179b1a1e6747c08aac19799a49861166e
100milliongold <gadian88@gmail.com>
2022-10-01 14:49:28 +0900

    인터셉터 사용하기

---
+++ a-nest/src/common/interceptors/undefinedToNull.interceptor.ts
@@ -0,0 +1,18 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class UndefinedToNullInterceptor implements NestInterceptor {
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<any>,
+  ): Observable<any> | Promise<Observable<any>> {
+    // 전부분
+    return next.handle().pipe((data) => (data === undefined ? null : data));
+  }
+}
